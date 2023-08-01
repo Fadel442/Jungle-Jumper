3e5bc8861fc502fd1696fe5021f84fb833602e9e
BlueMoonZ <79092830+Fadel442@users.noreply.github.com>
2023-08-01 15:30:36 +0700

    2

--- CoinBar.java
+++ CoinBar.java
@@ -25,15 +25,9 @@ public class CoinBar extends Actor
     private void updateImage() {
         GreenfootImage image = new GreenfootImage(getImage());
         image.clear();
-        int coinSpacing = 2; // Jarak antar koin (ganti angka ini sesuai keinginan)
-    int coinWidth = coinImage.getWidth(); // Mengambil lebar pixel gambar koin asli
-    int totalWidth = coins * coinWidth + (coins - 1) * coinSpacing;
-    int startX = (getImage().getWidth() - totalWidth) / 2;
-
-    for (int i = 0; i < coins; i++) {
-        int x = startX + (coinWidth + coinSpacing) * i;
-        image.drawImage(coinImage, x, 0); // Menggambar koin secara berderet dengan jarak yang telah ditentukan
-    }
+        for (int i = 0; i < coins; i++) {
+            image.drawImage(coinImage, i * (coinImage.getWidth() + 5), 0); // Menggambar koin secara berderet
+        }
         setImage(image);
     }
 
