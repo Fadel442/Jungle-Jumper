5bf8588396553fba290ba1ce00010823c094d7ef
BlueMoonZ <79092830+Fadel442@users.noreply.github.com>
2023-08-02 09:32:18 +0700

    up

--- LevelWorld.java
+++ LevelWorld.java
@@ -8,6 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class LevelWorld extends World
 {
+    private Jumper jumper;
 
     /**
      * Constructor for objects of class LevelWorld.
@@ -16,6 +17,22 @@ public class LevelWorld extends World
     public LevelWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(800, 450, 1);
+        
+        prepare();
+    }
+    
+    private void prepare() {
+        jumper = new Jumper();
+        addObject(jumper, 80, 100);
+        
+        addPlatform();
+    }
+    
+    private void addPlatform() {
+        Tiles tiles1 = new Tiles();
+        addObject(tiles1, 80, 140);
+        
+        
     }
 }
